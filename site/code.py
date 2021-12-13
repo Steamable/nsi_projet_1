302c7a3805e297700c6457db9af3cddf4a9439b5
Steamable <95611594+Steamable@users.noreply.github.com>
2021-12-13 08:09:24 +0100

    Update code.py

--- site/code.py
+++ site/code.py
@@ -1,7 +1,9 @@
-choix = int(input("choisis un nombre\n"))
+choix = int(input("choisis un nombre\n"))#on choisis le nombre a convertir en hieroglyphe
 
-def entier_vers_egypt(entier):
-    chiffres = [(1000000, "☺"),
+
+
+def entier_vers_egypt(entier):#on défini une fonction pour effectuer la conversion
+    chiffres = [(1000000, "☺"),#creation d'une liste pour stocker et utiliser les valeurs
             (100000, "♪"),
             (10000, "⌠"),
             (1000, "֍"),
@@ -9,13 +11,23 @@ def entier_vers_egypt(entier):
             (10, "∩"),
             (1, "I")]
 
-    list_egypt = []
 
-    for number, symbol in chiffres:
+
+    list_egypt = []#nouvelle liste pour ajouter les signes a la fin de la conversion
+    
+    
+
+    for number, symbol in chiffres:#boucle pour faire la conversion 
+    
         while entier >= number:
-            entier -= number
-            list_egypt.append(symbol)
+            entier -= number #tant que l'entier mis par l'utilisateur est supéerieur ou egal au nombre de la liste, on soustrait le nombre a l'entier choisi pour afficher dans l'ordre decroissant les signes 
+            
+            
+            
+            list_egypt.append(symbol)#ajoute les symbole dans la liste
 
-    return " ".join(list_egypt)
 
-print(entier_vers_egypt(choix))
+    return " ".join(list_egypt)#un espace entre chaque symbole
+
+
+print(entier_vers_egypt(choix))#affiche le resultat par rapport au nombre entré
